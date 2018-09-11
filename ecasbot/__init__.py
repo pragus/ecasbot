64744d6c29cbf9b86823ab8db01ea9af86801951
Roman Timofeev <cryptoloop@gmail.com>
2018-09-11 13:59:27 +0300

    big code rework

--- ecasbot/__init__.py
+++ ecasbot/__init__.py
@@ -25,6 +25,87 @@ import telebot
 from .settings import Settings
 
 
+class FmtMsg:
+    def __init__(self, text):
+        self.__t = text
+
+    def __call__(self, *args, **kwargs):
+        return self.__t.format(*args, **kwargs)
+
+
+class Messages:
+    Welcome = FmtMsg('Add me to supergroup and give me admin rights. I will try to block spammers automatically.')
+    Alog = FmtMsg('New user {} with ID {} has joined group. Score: {}.')
+    Restex = FmtMsg('Cannot restrict a new user with ID {} due to missing admin rights.')
+    Msgex = FmtMsg('Exception detected while handling spam message from {}.')
+    Notoken = FmtMsg('No API token entered. Cannot proceed. Fix this issue and run this bot again!')
+    Joinhex = FmtMsg('Failed to handle join message.')
+    Banned = FmtMsg('Permanently banned user with ID {} (score: {}).')
+    Msgrest = FmtMsg('Removed message from restricted user {} with ID {}.')
+    Amsgrm = FmtMsg('Admin {} ({}) removed message from user {} with ID {}.')
+    Amute = FmtMsg('Admin {} ({}) permanently muted user {} with ID {}.')
+    Aunres = FmtMsg('Admin {} ({}) removed all restrictions from user {} with ID {}.')
+    Aban = FmtMsg('Admin {} ({}) permanently banned user {} with ID {}.')
+    Admerr = FmtMsg('Failed to handle admin command.')
+    Chkme = FmtMsg('Checking of account {} successfully completed. Your score is: {}.')
+    Pmex = FmtMsg('Failed to handle command in private chat with bot.')
+
+
+class Perm:
+    New = dict(
+        can_send_messages=True,
+        can_send_media_messages=False,
+        can_send_other_messages=False,
+        can_add_web_page_previews=False
+        )
+    Unrest = dict(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_other_messages=True,
+        can_add_web_page_previews=True
+        )
+    Rest = dict(
+        can_send_messages=False, can_send_media_messages=False, can_send_other_messages=False,
+        can_add_web_page_previews=False
+        )
+
+
+class Msg:
+    def __init__(self, message, settings):
+        self.__m = message
+        self.__s = settings
+
+    @property
+    def username(self):
+        if self.__m.reply_to_message.new_chat_member:
+            return self.__m.reply_to_message.new_chat_member.first_name
+        else:
+            return self.__m.reply_to_message.from_user.first_name
+
+    @property
+    def userid(self):
+        if self.__m.reply_to_message.new_chat_member:
+            return self.__m.reply_to_message.new_chat_member.id
+        else:
+            return self.__m.reply_to_message.from_user.id
+
+    @property
+    def is_forward(self):
+        return self.__m.forward_from or self.__m.forward_from_chat
+
+    @property
+    def is_entities_ok(self):
+        if self.__m.entities:
+            for entity in self.__m.entities:
+                if entity.type in self.__s.restent:
+                    return False
+        return True
+
+    @staticmethod
+    def is_private_chat(message) -> bool:
+        return message.chat.type == 'private'
+
+
 class ASBot:
     def __check_restricted_user(self, m) -> bool:
         usr = self.bot.get_chat_member(m.chat.id, m.from_user.id)
@@ -33,30 +114,7 @@ class ASBot:
     def __check_admin_feature(self, m) -> bool:
         usr = self.bot.get_chat_member(m.chat.id, m.from_user.id)
         return m.chat.type == 'supergroup' and (
-                    m.from_user.id in self.__settings.admins or usr.status == 'administrator')
-
-    @staticmethod
-    def __check_private_chat(message) -> bool:
-        return message.chat.type == 'private'
-
-    @staticmethod
-    def __get_actual_username(message):
-        return message.reply_to_message.new_chat_member.first_name if message.reply_to_message.new_chat_member else message.reply_to_message.from_user.first_name
-
-    @staticmethod
-    def __get_actual_userid(message):
-        return message.reply_to_message.new_chat_member.id if message.reply_to_message.new_chat_member else message.reply_to_message.from_user.id
-
-    @staticmethod
-    def __check_message_forward(message):
-        return message.forward_from or message.forward_from_chat
-
-    def __check_message_entities(self, message):
-        if message.entities:
-            for entity in message.entities:
-                if entity.type in self.__settings.restent:
-                    return True
-        return False
+                m.from_user.id in self.__settings.admins or usr.status == 'administrator')
 
     def __score_user(self, fname, lname) -> int:
         # Setting default score to 0...
@@ -83,20 +141,20 @@ class ASBot:
 
     def runbot(self) -> None:
         # Initialize command handlers...
-        @self.bot.message_handler(func=self.__check_private_chat, commands=['start'])
+        @self.bot.message_handler(func=Msg.is_private_chat, commands=['start'])
         def handle_start(message):
             try:
-                self.bot.send_message(message.chat.id, self.__msgs['as_welcome'])
+                self.bot.send_message(message.chat.id, Messages.Welcome())
             except:
-                self.__logger.exception(self.__msgs['as_pmex'])
+                self.__logger.exception(Messages.Pmex())
 
-        @self.bot.message_handler(func=self.__check_private_chat, commands=['checkme'])
+        @self.bot.message_handler(func=Msg.is_private_chat, commands=['checkme'])
         def handle_checkme(message):
             try:
                 score = self.__score_user(message.from_user.first_name, message.from_user.last_name)
-                self.bot.send_message(message.chat.id, self.__msgs['as_chkme'].format(message.from_user.id, score))
+                self.bot.send_message(message.chat.id, Messages.Chkme(message.from_user.id, score))
             except:
-                self.__logger.exception(self.__msgs['as_pmex'])
+                self.__logger.exception(Messages.Pmex())
 
         @self.bot.message_handler(func=self.__check_admin_feature, commands=['remove', 'rm'])
         def handle_remove(message):
@@ -105,62 +163,88 @@ class ASBot:
                 if message.reply_to_message:
                     self.bot.delete_message(message.chat.id, message.reply_to_message.message_id)
                     self.__logger.warning(
-                        self.__msgs['as_amsgrm'].format(message.from_user.first_name, message.from_user.id,
-                                                        message.reply_to_message.from_user.first_name,
-                                                        message.reply_to_message.from_user.id))
+                        Messages.Amsgrm(
+                            message.from_user.first_name,
+                            message.from_user.id,
+                            message.reply_to_message.from_user.first_name,
+                            message.reply_to_message.from_user.id
+                            )
+                        )
             except:
-                self.__logger.exception(self.__msgs['as_admerr'])
+                self.__logger.exception(Messages.Admerr())
 
         @self.bot.message_handler(func=self.__check_admin_feature, commands=['ban', 'block'])
         def handle_banuser(message):
             try:
                 if message.reply_to_message:
-                    username = self.__get_actual_username(message)
-                    userid = self.__get_actual_userid(message)
-                    if message.from_user.id != userid:
-                        self.bot.kick_chat_member(message.chat.id, userid)
+                    msg = Msg(message, self.__settings)
+                    if message.from_user.id != msg.userid:
+                        self.bot.kick_chat_member(message.chat.id, msg.userid)
                         self.__logger.warning(
-                            self.__msgs['as_aban'].format(message.from_user.first_name, message.from_user.id, username,
-                                                          userid))
+                            Messages.Aban(
+                                message.from_user.first_name,
+                                message.from_user.id,
+                                msg.username,
+                                msg.userid
+                                )
+                            )
             except:
-                self.__logger.exception(self.__msgs['as_admerr'])
+                self.__logger.exception(Messages.Admerr())
 
         @self.bot.message_handler(func=self.__check_admin_feature, commands=['restrict', 'mute'])
         def handle_muteuser(message):
             try:
                 if message.reply_to_message:
-                    username = self.__get_actual_username(message)
-                    userid = self.__get_actual_userid(message)
-                    if message.from_user.id != userid:
-                        self.bot.restrict_chat_member(message.chat.id, userid, until_date=int(time.time()),
-                                                      can_send_messages=False, can_send_media_messages=False,
-                                                      can_send_other_messages=False, can_add_web_page_previews=False)
+                    msg = Msg(message, self.__settings)
+                    if message.from_user.id != msg.userid:
+                        self.bot.restrict_chat_member(
+                            message.chat.id, msg.userid,
+                            until_date=int(time.time()),
+                            **Perm.Rest
+                            )
                         self.__logger.warning(
-                            self.__msgs['as_amute'].format(message.from_user.first_name, message.from_user.id, username,
-                                                           userid))
+                            Messages.Amute(
+                                message.from_user.first_name,
+                                message.from_user.id,
+                                msg.username,
+                                msg.userid
+                                )
+                            )
             except:
-                self.__logger.exception(self.__msgs['as_admerr'])
+                self.__logger.exception(Messages.Admerr())
 
         @self.bot.message_handler(func=self.__check_admin_feature, commands=['unrestrict', 'un'])
         def handle_unrestrict(message):
             try:
                 if message.reply_to_message:
-                    self.bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
-                                                  can_send_messages=True, can_send_media_messages=True,
-                                                  can_send_other_messages=True, can_add_web_page_previews=True)
+                    self.bot.restrict_chat_member(
+                        message.chat.id,
+                        message.reply_to_message.from_user.id,
+                        **Perm.Unrest
+                        )
                     self.__logger.warning(
-                        self.__msgs['as_aunres'].format(message.from_user.first_name, message.from_user.id,
-                                                        message.reply_to_message.from_user.first_name,
-                                                        message.reply_to_message.from_user.id))
+                        Messages.Aunres(
+                            message.from_user.first_name,
+                            message.from_user.id,
+                            message.reply_to_message.from_user.first_name,
+                            message.reply_to_message.from_user.id
+                            )
+                        )
             except:
-                self.__logger.exception(self.__msgs['as_admerr'])
+                self.__logger.exception(Messages.Admerr())
 
         @self.bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
         def handle_join(message):
             try:
                 # Check user profile using our score system...
                 score = self.__score_user(message.new_chat_member.first_name, message.new_chat_member.last_name)
-                self.__logger.info(self.__msgs['as_alog'].format(message.new_chat_member.first_name, message.new_chat_member.id, score))
+                self.__logger.info(
+                    Messages.Alog(
+                        message.new_chat_member.first_name,
+                        message.new_chat_member.id,
+                        score
+                        )
+                    )
                 try:
                     # If user get score >= 100 - ban him, else - restrict...
                     if score >= 100:
@@ -171,29 +255,37 @@ class ASBot:
                         if message.from_user.id != message.new_chat_member.id:
                             self.bot.kick_chat_member(message.chat.id, message.from_user.id)
                         # Writing information to log...
-                        self.__logger.warning(self.__msgs['as_banned'].format(message.new_chat_member.id, score))
+                        self.__logger.warning(Messages.Banned(message.new_chat_member.id, score))
                     else:
                         # Restrict all new users for specified in config time...
-                        self.bot.restrict_chat_member(message.chat.id, message.new_chat_member.id,
-                                                      until_date=int(time.time()) + self.__settings.bantime,
-                                                      can_send_messages=True, can_send_media_messages=False,
-                                                      can_send_other_messages=False, can_add_web_page_previews=False)
+                        self.bot.restrict_chat_member(
+                            message.chat.id,
+                            message.new_chat_member.id,
+                            until_date=int(time.time()) + self.__settings.bantime,
+                            **Perm.New
+                            )
                 except Exception:
                     # We have no admin rights, show message instead...
-                    self.__logger.exception(self.__msgs['as_restex'].format(message.from_user.id))
+                    self.__logger.exception(Messages.Restex(message.from_user.id))
             except Exception:
-                self.__logger.exception(self.__msgs['as_joinhex'])
+                self.__logger.exception(Messages.Joinhex())
 
         @self.bot.message_handler(func=self.__check_restricted_user)
         @self.bot.edited_message_handler(func=self.__check_restricted_user)
         def handle_msg(message):
             try:
+                msg = Msg(message, self.__settings)
                 # Removing messages from restricted members...
-                if self.__check_message_entities(message) or self.__check_message_forward(message):
+                if msg.is_forward or not msg.is_entities_ok:
                     self.bot.delete_message(message.chat.id, message.message_id)
-                    self.__logger.info(self.__msgs['as_msgrest'].format(message.from_user.first_name, message.from_user.id))
+                    self.__logger.info(
+                        Messages.Msgrest(
+                            message.from_user.first_name,
+                            message.from_user.id
+                            )
+                        )
             except Exception:
-                self.__logger.exception(self.__msgs['as_msgex'].format(message.from_user.id))
+                self.__logger.exception(Messages.Msgex(message.from_user.id))
 
         # Run bot forever...
         self.bot.polling(none_stop=True)
@@ -203,23 +295,6 @@ class ASBot:
         self.__schema = 1
         self.__logger = logging.getLogger(__name__)
         self.__settings = Settings(self.__schema)
-        self.__msgs = {
-            'as_welcome': 'Add me to supergroup and give me admin rights. I will try to block spammers automatically.',
-            'as_alog': 'New user {} with ID {} has joined group. Score: {}.',
-            'as_restex': 'Cannot restrict a new user with ID {} due to missing admin rights.',
-            'as_msgex': 'Exception detected while handling spam message from {}.',
-            'as_notoken': 'No API token entered. Cannot proceed. Fix this issue and run this bot again!',
-            'as_joinhex': 'Failed to handle join message.',
-            'as_banned': 'Permanently banned user with ID {} (score: {}).',
-            'as_msgrest': 'Removed message from restricted user {} with ID {}.',
-            'as_amsgrm': 'Admin {} ({}) removed message from user {} with ID {}.',
-            'as_amute': 'Admin {} ({}) permanently muted user {} with ID {}.',
-            'as_aunres': 'Admin {} ({}) removed all restrictions from user {} with ID {}.',
-            'as_aban': 'Admin {} ({}) permanently banned user {} with ID {}.',
-            'as_admerr': 'Failed to handle admin command.',
-            'as_chkme': 'Checking of account {} successfully completed. Your score is: {}.',
-            'as_pmex': 'Failed to handle command in private chat with bot.'
-        }
         if not self.__settings.tgkey:
-            raise Exception(self.__msgs['as_notoken'])
+            raise Exception(Messages.Notoken())
         self.bot = telebot.TeleBot(self.__settings.tgkey)
